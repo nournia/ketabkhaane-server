06325a8324b7df86510d6efe9c31ba0e0bc4614e
Alireza Nourian <alireza.nournia@gmail.com>
2012-08-11 07:31:39 +0430

    register new client

--- backend.php
+++ backend.php
@@ -1,7 +1,18 @@
 <?php require('begin.php') ?>
 <?php
+	function updateLicense($id) {
+		$days = 100;
+		mysql_query("update reghaabats set license = sha1(adddate(now(), $days)) where id = $id");
+		return mysql_fetch_row(mysql_query("select license from reghaabats where id = $id"))[0];
+	}
+
 	// Register
-	
+	if (isset($_POST['register'])) {
+		mysql_query('insert into reghaabats (title) values ("")');
+		$id = mysql_insert_id();
+		$license = updateLicense($id);
+		echo $id .','. $license;
+	}
 
 	// Receive
 
--- db.php
+++ db.php
@@ -5,6 +5,10 @@ if (isset($_GET['rebuild'])) {
 
 $commands = array(
 'drop table if exists reghaabats',
+'drop table if exists logs'
+);
+
+$creates = array(
 'create table reghaabats (
 	id integer not null primary key auto_increment,
 	title varchar(255) not null,
@@ -12,7 +16,6 @@ $commands = array(
 	synced_at timestamp null default null,
 	license varchar(255) null default null)',
 
-'drop table if exists logs',
 'create table logs (
 	reghaabat_id integer not null,
 	table_name enum("users", "accounts", "permissions", "authors", "publications", "files", "matches", "questions", "answers", "library", "supports", "scores", "transactions", "open_categories", "open_scores", "roots", "branches", "objects", "borrows") not null,
@@ -20,9 +23,12 @@ $commands = array(
 	row_id integer not null,
 	row_data text null,
 	user_id integer null default null,
-	created_at timestamp null default null
-)'
+	created_at timestamp null default null)'
 );
+$table_conf = ' collate=utf8_general_ci engine=InnoDB';
+
+foreach ($creates as $command)
+	$commands[] = $command . $table_conf;
 
 foreach ($commands as $command)
 	mysql_query($command);
