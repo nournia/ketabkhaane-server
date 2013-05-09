63922427e961f832409f032598102581db6b37a5
Alireza Nourian <alireza.nournia@gmail.com>
2013-05-09 22:09:36 +0430

    add laravel framework
    remove all old codes

--- php/server/end.php
+++
@@ -1,3 +0,0 @@
-<?php
-if (isset($connection))
-	mysql_close($connection);
--- php/server/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-	<title>Rehgaabat</title>
-</head>
-<body>
-	<h2>Rehaabat Server</h2>
-	<p>This site prepared for collecting data from <a href="http://reghaabat.ir">reghaabat</a> clients.</p>
-</body>
-</html>
