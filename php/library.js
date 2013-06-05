df456fdd4cab81fda9b0e11eaccd1ddcf5ac2dba
Alireza Nourian <alireza.nournia@gmail.com>
2013-06-05 12:20:25 +0430

    add continue button to book list

--- php/library.js
+++ php/library.js
@@ -9,10 +9,11 @@ var Items = Backbone.Collection.extend({
 	model: Item,
 
 	getRows: function(branch, query) {
-		if (!query)
-			return _(this.filter(function(item) {
-				return item.get('branch') == branch;
-			}));
+		if (!query) {
+			if (branch == 0)
+				return this.toArray();
+			return this.where({branch: Number(branch)})
+		}
 
 		return _(this.filter(function(item) {
 			return item.text().indexOf(query) >= 0;
@@ -38,6 +39,7 @@ var ItemsView = Backbone.View.extend({
 		this.libraryId = options['libraryId'];
 		this.query = '';
 		this.branch = '';
+		this.show = 100;
 
 		$.ajax({
 			url: 'data/object_list/'+ this.libraryId,
@@ -47,6 +49,7 @@ var ItemsView = Backbone.View.extend({
 				// fill branch select
 				selector = $('#branch');
 				selector.empty();
+				selector.append('<option value="0">همه</option>');
 				_.each(data['branches'], function(item) {
 					selector.append('<option value="'+ item[0] +'">'+ item[1] +'</option>');
 				});
@@ -58,7 +61,7 @@ var ItemsView = Backbone.View.extend({
 				});
 
 				// render
-				selector.val(111).change();
+				selector.val(0).change();
 				$('.alert').hide();
 				$('#object-browser').fadeIn();
 			},
@@ -73,19 +76,28 @@ var ItemsView = Backbone.View.extend({
 		if (branch != this.branch || query != this.query) {
 			this.branch = branch;
 			this.query = query;
+			this.show = 100;
 			this.render();
 		}
 	},
 	render: function() {
+		items = this.collection.getRows(this.branch, this.query);
+
 		var tbody = this.$el.find('tbody');
 		tbody.empty();
-
-		items = this.collection.getRows(this.branch, this.query).first(200);
-		_.each(items, function (item) {
+		_.each(_.first(items, this.show), function (item) {
 			var itemView = new ItemView({model: item});
 			tbody.append(itemView.render().el);
 		});
-
 		tbody.find('tr.warning').tooltip({placement: 'left', title: 'امانت داده شده', container: 'body'});
+
+		this.$el.find('#continue').remove();
+		if (items.length > this.show) {
+			this.$el.append('<button id="continue" class="btn" rel="'+ this.show +'">ادامه فهرست</button>');
+			this.$el.find('#continue').click(function() {
+				itemsView.show = Number($(this).attr('rel')) + 100;
+				itemsView.render();
+			});
+		}
 	}
 });
